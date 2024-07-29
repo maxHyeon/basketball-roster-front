13c9f519156e3cab02dc11303300546999da1917
maxHyeon <maxhyeon.park@gmail.com>
2024-07-29 17:12:17 +0900

    next-auth readd

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -4,6 +4,7 @@ import { Fragment } from 'react';
 import { usePathname } from 'next/navigation';
 import { Disclosure, Menu, Transition } from '@headlessui/react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+import { signOut } from 'next-auth/react';
 import Image from 'next/image';
 import awsRumClient from '@/aws/cwr/cwr';
 const navigation = [
@@ -79,7 +80,7 @@ export default function Navbar({ user }: { user: any }) {
                                 active ? 'bg-gray-100' : '',
                                 'flex w-full px-4 py-2 text-sm text-gray-700'
                               )}
-                              onClick={() => {}}
+                              onClick={() => signOut()}
                             >
                               Sign out
                             </button>
@@ -160,7 +161,7 @@ export default function Navbar({ user }: { user: any }) {
                   </div>
                   <div className="mt-3 space-y-1">
                     <button
-                      onClick={() => {}}
+                      onClick={() => signOut()}
                       className="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800"
                     >
                       Sign out
